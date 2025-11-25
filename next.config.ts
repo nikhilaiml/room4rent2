e143aefd67602bb42615ad52309e51f84cc6228a
nikhilaiml <nikhilmaurya8887@gmail.com>
2025-11-25 17:52:02 +0530

    hhghg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
   },
